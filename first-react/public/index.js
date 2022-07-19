6868b8d33c3c8ac1b7492ef6d8140151c79fdc3e
MsPreciousO <opalcreations68@gmail.com>
2022-07-18 21:02:02 -0400

    Starting to work on my First React

---
+++ first-react/public/index.js
@@ -0,0 +1,7 @@
+// ReactDOM.render(<h1> Hello, I am working on First React</h1>, document.getElementById('root'))
+
+// ReactDOM.render(<p>Hi Beautiful, See you soon</p>, document.getElementById('root'))
+
+ReactDOM.render(
+<ul> <li>Prepare acct</li><li>Upload acct</li><li>Submit acct</li></ul>,
+document.getElementById('root'))
---
+++ first-react/src/index.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+// ReactDOM.render(<h1> Hello, I am working on First React</h1>, document.getElementById('root'))
+
+// ReactDOM.render(<p>Hi Beautiful, See you soon</p>, document.getElementById('root'))
+
+// ReactDOM.render(<h1> Hello, I am working on First React</h1>, document.getElementById('root'))
